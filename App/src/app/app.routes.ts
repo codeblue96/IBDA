344035ce40dad7d07f6596c3973fd91c7c54d566
Muhammad Ahmad <muhammad.ahmad96@outlook.com>
2024-12-04 12:17:49 +0500

    blog-header-ready

--- App/src/app/app.routes.ts
+++ App/src/app/app.routes.ts
@@ -8,6 +8,8 @@ import { AiMlComponent } from './our-services/ai-ml/ai-ml.component';
 import { WebDevComponent } from './our-services/web-dev/web-dev.component';
 import { EdTechComponent } from './our-services/ed-tech/ed-tech.component';
 import { EmbeddedComponent } from './our-services/embedded/embedded.component';
+import { BlogReadingComponent } from './blog-reading/blog-reading.component';
+import { BlogComponent } from './blog/blog.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent, title: 'Home' },
@@ -19,4 +21,6 @@ export const routes: Routes = [
   { path: 'web-dev', component: WebDevComponent, title: 'Web-Development' },
   { path: 'ed-tech', component: EdTechComponent, title: 'Ed-Tech' },
   { path: 'embedded', component: EmbeddedComponent, title: 'Embedded' },
+  { path: 'blog-reading', component: BlogReadingComponent, title: 'Blog-reading' },
+  { path: 'blogs', component: BlogComponent, title: 'Blogs' },
 ];
---
+++ App/src/app/blog-reading/blog-reading.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-blog-reading',
+  standalone: true,
+  imports: [],
+  templateUrl: './blog-reading.component.html',
+  styleUrl: './blog-reading.component.css'
+})
+export class BlogReadingComponent {
+
+}
