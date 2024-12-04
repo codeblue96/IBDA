2a14b13833d748a54da5bcd7e7c19eaf85eeafed
Muhammad Ahmad <muhammad.ahmad96@outlook.com>
2024-12-04 13:53:32 +0500

    before adding search system

--- App/src/app/blog/blog.component.ts
+++ App/src/app/blog/blog.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-blog',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './blog.component.html',
-  styleUrl: './blog.component.css'
+  styleUrl: './blog.component.css',
 })
-export class BlogComponent {
+export class BlogComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
+    this.setupCarousel('carouselExampleCaptions2');
+  }
+  setupCarousel(carouselId: string): void {
+    const carousel = document.getElementById(carouselId);
+    if (carousel) {
+      const nextButton = carousel.querySelector('.carousel-control-next');
+      const prevButton = carousel.querySelector('.carousel-control-prev');
 
+      if (nextButton) {
+        nextButton.addEventListener('click', () => {
+          console.log('Next button clicked');
+        });
+      }
+      if (prevButton) {
+        prevButton.addEventListener('click', () => {
+          console.log('Previous button clicked');
+        });
+      }
+    }
+  }
 }
